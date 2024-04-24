0e44e0131c9f01731eed760f09ea944514c4d4f4
Willder Canabrava <willcanabrava@gmail.com>
2024-04-24 16:49:52 -0300

    Adicionando código

--- src/PasswordIdentification.java
+++ src/PasswordIdentification.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class PasswordIdentification {
+import java.util.Scanner;
+
+public class PasswordIdentification {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite sua senha: ");
+        String password = scanner.next();
+
+        int count = 1;
+
+        while (!password.equals("CRF1895") && count <= 2) {
+            System.out.println("Digite novamente sua senha: \nTentativas restantes: " + (3 - count));
+            password = scanner.next();
+            count++;
+        }
+
+        if (password.equals("CRF1895")) {
+            System.out.println("Acesso liberado!");
+        } else {
+            System.out.println("Acesso bloqueado!");
+        }
+
+    }
+
 }
